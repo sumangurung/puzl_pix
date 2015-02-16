06a909d873a73db18902b65ba9a2ddf255433eb0
Suman Gurung <sumangurung@gmail.com>
2015-02-16 12:17:15 -0500

    Scores can be created, kind of

---
+++ spec/requests/store_game_score_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "game score" do
+  it "stores the game score" do
+    request_headers = {
+      "Accept" => "application/json",
+      "Content-Type" => "application/json"
+    }
+
+    score_params = {
+      score: {
+        user_id: '123',
+        game_id: 'awesomegame1',
+        cols: '3',
+        date: "02/02/2014",
+        difficulty: "very",
+        game_mode: "easy",
+        moves: "20",
+        rows: '3',
+        time: '140'
+      }
+    }
+
+    post '/v1/scores', score_params.to_json, request_headers
+
+    expect(response.status).to eq 201
+  end
+end
