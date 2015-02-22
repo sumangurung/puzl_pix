28e4e808756efe285360593d0ba84c3228392dc2
Suman Gurung <sumangurung@gmail.com>
2015-02-22 17:59:05 -0500

    Actually store player information in the database

--- app/controllers/v1/players_controller.rb
+++ app/controllers/v1/players_controller.rb
@@ -2,8 +2,16 @@ module V1
   class PlayersController < ApplicationController
 
     def create
-      @player = Player.create!(params[:player])
+      @player = Player.create!(player_params)
       render template: '/players/show', status: :created
+    end
+
+    private
+
+    def player_params
+      permitted_attributes = %i(fb_id first_name last_name
+      username)
+      params.require(:player).permit(*permitted_attributes)
     end
   end
 end
--- spec/requests/players_spec.rb
+++ spec/requests/players_spec.rb
@@ -15,16 +15,23 @@ RSpec.describe "player registration info" do
       "Authorization" => "Token token=#{token}"
     }
     player_params = {
-      player: { fb_id: 123, first_name: "Jimmy", last_name: "Johnson", username: "jj" }
+      player: { fb_id: "123", first_name: "Jimmy", last_name: "Johnson", username: "jj" }
     }.to_json
 
     post '/v1/players', player_params, request_headers
 
     expect(response.status).to eq 201
     player = JSON.parse(response.body)['player']
-    expect(player['fb_id']).to eq(123)
+    expect(player['fb_id']).to eq("123")
     expect(player['first_name']).to eq("Jimmy")
     expect(player['last_name']).to eq("Johnson")
     expect(player['username']).to eq("jj")
+
+    players = Player.all
+    expect(players.count).to eq 1
+    expect(players.first.fb_id).to eq("123")
+    expect(players.first.first_name).to eq("Jimmy")
+    expect(players.first.last_name).to eq("Johnson")
+    expect(players.first.username).to eq("jj")
   end
 end
