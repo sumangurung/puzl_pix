b235b99e6d4e220cc254aed434ffcd1bc446bbe4
Suman Gurung <sumangurung@gmail.com>
2017-11-06 16:58:36 -0500

    Organize more stuff to traditional MVC

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,20 +1,3 @@
-class Challenge
-  attr_reader :challengees
-  def initialize(params)
-    @date = params['date']
-    @picture_url = params['picture_url']
-    @thumb_url = params['thumb_url']
-    @game_id = params['game_id']
-    @challengees = params['challengees'] || []
-  end
-
-  def attributes
-    {
-      date: @date,
-      picture_url: @picture_url,
-      thumb_url: @thumb_url,
-      challengees: @challengees,
-      game_id: @game_id
-    }
-  end
+class Challenge < ActiveRecord::Base
+  has_many :challengees, class_name: "Challengee"
 end
--- app/services/challenges.rb
+++ app/services/challenges.rb
@@ -2,13 +2,19 @@ require 'arel'
 module Challenges
   def self.fetch(fb_id)
     challengees_table = Arel::Table.new(Challengee.table_name)
-    Persistence::Challenge.joins(:challengees)
+    Challenge.joins(:challengees)
       .where(challengees_table[:fb_id].eq(fb_id))
   end
 
   def self.create!(params)
-    challenge = Challenge.new(params.with_indifferent_access)
-    Persistence::Challenge.save!(challenge)
+    attributes = params.dup
+    challengees = attributes.delete(:challengees) || []
+
+    challenge = Challenge.new(attributes)
+    challengees.each do |challengee_id|
+      challenge.challengees.build(fb_id: challengee_id)
+    end
+    challenge.save!
     ChallengeNotifier.notify(challenge)
   end
 
