2a5dd31aaed7de1087a95f1180d9c1427cf2b678
Ryan Osborn <puzlpix@gmail.com>
2017-12-10 14:39:23 -0500

    Add challenge/id GET route

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
       end
     end
 
-    resources :challenges, only: [:create, :index]
+    resources :challenges, only: [:create, :index, :show]
   end
 
   namespace :v1 do
