f7e0f09ed2a327ab44ea9a17c643528daba1254e
Ryan Osborn <puzlpix@gmail.com>
2017-12-13 14:04:54 -0500

    Change stage name

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,5 +2,5 @@ set :application, "puzlpix_staging"
 set :deploy_to, "/var/www/html/#{fetch :application}"
 set :server_name, "staging.puzlpix.com"
 set :rails_env, 'staging'
-set :stage, "production"
+set :stage, "staging"
 set :branch, "master"
